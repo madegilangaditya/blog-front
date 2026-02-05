82b34b0767142eb7ef4fac2cabef62a82093729c
gilang aditya <madegilangaditya32@gmail.com>
2026-02-05 13:19:35 +0800

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Hero from "./components/Hero";
+
+export default function Home() {
+  return (
+    <main>
+      {/* banner home */}
+      {/* <section className="h-screen">
+        
+      </section> */}
+      <Hero />
+      <div className="h-[200vh]" />
+    </main>
+  );
+}
